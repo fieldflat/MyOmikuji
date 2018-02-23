299fc577e771b8750eaaa295544f7274f91c0660
平田智紀 <tomonorihirata@hiratatomoki-no-MacBook-Air.local>
2018-02-23 15:44:46 +0900

    おみくじ7

--- js/main.js
+++ js/main.js
@@ -4,7 +4,18 @@
   var btn = document.getElementById('btn');
 
   btn.addEventListener('click', function(){
-    this.textContent = 'hit!';
+    var n = Math.floor(Math.random() * 3);
+    switch(n){
+      case 0:
+        this.textContent = '全然ダメ';
+        break;
+      case 1:
+        this.textContent = 'まあまあ';
+        break;
+      case 2:
+        this.textContent = 'すげえじゃん';
+    }
+    //this.textContent = n;
   });
   btn.addEventListener('mousedown', function(){
     this.className = 'pushed';
